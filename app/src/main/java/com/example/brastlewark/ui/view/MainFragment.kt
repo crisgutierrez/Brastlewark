6dc905067b5593d4a3fab19a5b4c92d94d5aff50
Cristian <crisgutierrez014@gmail.com>
2021-02-06 14:44:35 +0100

    Set up navigation
    - Created navigation graph, the fragments that we will use and the navigation between them.

--- app/src/main/java/com/example/brastlewark/ui/view/MainFragment.kt
+++ app/src/main/java/com/example/brastlewark/ui/view/MainFragment.kt
@@ -8,19 +8,18 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.brastlewark.R
 import com.example.brastlewark.util.DataState
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.main_fragment.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
 class MainFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = MainFragment()
-    }
-
+    private val navController by lazy { findNavController(this) }
     private val viewModel: MainViewModel by viewModels()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -28,11 +27,12 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         setObservers()
         viewModel.setStateEvent(MainStateEvent.GetGnomeEvents)
+
+        buttonId.setOnClickListener { navController.navigate(R.id.action_mainFragment_to_gnomeDetailsFragment) }
     }
 
     // region PRIVATE METHODS -----------------------------------------------------------------------
